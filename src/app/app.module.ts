c55811a3b5795b403f3b13408551de70fad3e97d
Matheus FPS <matheus.filipi@selgron.com.br>
2024-03-14 16:56:57 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
+import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
+import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductionService } from './services/production.service';
+import { IhmStatusService } from './services/ihm-status.service';
+import { WidgetComponent } from './widget/widget.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    SalesByMonthComponent,
+    SalesByCategoryComponent,
+    LastFewTransactionsComponent,
+    TopThreeProductsComponent,
+    SideNavComponent,
+    WidgetComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    ChartModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ProductionService, IhmStatusService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
